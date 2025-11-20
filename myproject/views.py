7850c5932f28515a4083a2f8fe62b8f235d46f84
devjuwel792 <developerjuwel792@gmail.com>
2025-11-20 15:07:35 +0600

    `Updated Django project settings and views to use string quotes consistently`

--- myproject/views.py
+++ myproject/views.py
@@ -3,11 +3,23 @@ from django.shortcuts import render
 
 
 def aboutUs(request):
+
     return HttpResponse("Welcome to about page .")
+
+
 def course(request):
     return HttpResponse("Welcome to about page .")
-def courseDetail(request,id):
+
+
+def courseDetail(request, id):
     return HttpResponse(id)
+
+
 def homePage(request):
-    return render(request, "index.html")
-    
+    data = {
+        "title": "Home new",
+        "subtitle": "Bangladesh is a small country",
+        "skills": [10, 20, 30, 40.55, 55, 67, 76, 34, 32, 23],
+        "student_details": [{"name": "Juwel Rana", "id": "4225020"}],
+    }
+    return render(request, "index.html", data)

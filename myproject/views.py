67698b0e86b44c149d83d74cdac7d05ab85f6dbd
devjuwel792 <developerjuwel792@gmail.com>
2025-11-20 15:49:56 +0600

    `Updated project files with new template code`

--- myproject/views.py
+++ myproject/views.py
@@ -16,10 +16,4 @@ def courseDetail(request, id):
 
 
 def homePage(request):
-    data = {
-        "title": "Home new",
-        "subtitle": "Bangladesh is a small country",
-        "skills": [10, 20, 30, 40.55, 55, 67, 76, 34, 32, 23],
-        "student_details": [{"name": "Juwel Rana", "id": "4225020"}],
-    }
-    return render(request, "index.html", data)
+    return render(request, "index.html")
